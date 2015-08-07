7c2220b87ac14476f8677c7cf93610080d9f1350
Gale Van Rossem <galeforcevr@gmail.com>
2015-08-06 21:34:50 -0500

    added users model and database migration

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    user_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=20)
