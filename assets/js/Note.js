d2ea793c3677db8fb6aaffb8522667f66a2b4d50
Gale Van Rossem <galeforcevr@gmail.com>
2015-09-07 21:02:30 -0500

    played with react

---
+++ assets/js/Note.js
@@ -0,0 +1,69 @@
+var Note = React.createClass({
+    getInitialState: function() {
+        return {editing: false}
+    },
+    edit: function() {
+        this.setState({editing: true});
+    },
+    save: function() {
+        var val = this.refs.newText.getDOMNode().value;
+        alert("TODO: Save note value" + val);
+        this.setState({editing: false});
+    },
+    remove: function() {
+        alert('removing note');
+    },
+    renderDisplay: function() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
+                </span>
+            </div>
+            );
+    },
+    renderForm: function() {
+        return (
+            <div className="note">
+                <textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
+                <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            </div>
+            );
+    },
+    render: function() {
+        if (this.state.editing) {
+            return this.renderForm();
+        } else {
+            return this.renderDisplay();
+        }
+    }
+});
+
+React.render(<Note>Hello World</Note>,
+    document.getElementById('react-test'));
+
+var Checkbox = React.createClass({
+    getInitialState: function() {
+        return {checked: false}
+    },
+    handleCheck: function() {
+        this.setState({checked: !this.state.checked})
+    },
+    render: function() {
+        var msg;
+        if (this.state.checked) {
+            msg = "checked";
+        } else {
+            msg = "unchecked";
+        }
+        return (<div>
+                <input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked}/>
+                <p>This box is {msg}.</p>
+            </div>);
+    }
+});
+
+React.render(<Checkbox />,
+    document.getElementById('react-container'));
