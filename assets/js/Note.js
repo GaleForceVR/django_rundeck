e9b93fce23288184c172ec155c1cd9999f92842b
Gale Van Rossem <galeforcevr@gmail.com>
2015-10-29 19:56:23 -0500

    played around with React.js

--- assets/js/Note.js
+++ assets/js/Note.js
@@ -41,7 +41,24 @@ var Note = React.createClass({
     }
 });
 
-React.render(<Note>Hello World</Note>,
+var Board = React.createClass({
+    propTypes: {
+        count: function(props, propName) {
+            if (typeof props[propName] !== "number"){
+                return new Error('The count property must be a number');
+            }
+            if  (props[propName] > 100) {
+                return new Error("Creating " + props[propName] + " notes is ridiculous");
+            }
+        }
+    },
+
+    render: function() {
+        return <div className="board">{this.props.count}</div>
+    }
+});
+
+React.render(<Board count={10}/>,
     document.getElementById('react-test'));
 
 var Checkbox = React.createClass({
